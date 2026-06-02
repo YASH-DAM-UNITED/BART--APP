ce4945d49aee84847e11d7ed1b57ba76907a9fd5
YASH-DAM-UNITED <data@damunited.sa>
2026-06-02 15:44:28 +0300

    Update staff_alignment.py

--- Bart--App-main/pages/staff_alignment.py
+++ Bart--App-main/pages/staff_alignment.py
@@ -7,12 +7,12 @@ from datetime import datetime, date
 
 import pytz
 
-st.sidebar.write(f"Server Time (Jeddah): {now.strftime('%H:%M')}")
-st.sidebar.write(f"Minutes Calculation: {now_min}")
+
 saudi_tz = pytz.timezone("Asia/Riyadh")
 now = datetime.now(saudi_tz)
 now_min = now.hour * 60 + now.minute
-
+st.sidebar.write(f"Server Time (Jeddah): {now.strftime('%H:%M')}")
+st.sidebar.write(f"Minutes Calculation: {now_min}")
 st.set_page_config(
     layout="wide",
     page_title="Ops Control Center",

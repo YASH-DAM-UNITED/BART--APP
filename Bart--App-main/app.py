3864a5ade7f7f9fcf983b9e293c66be36b347b85
YASH-DAM-UNITED <data@damunited.sa>
2026-06-04 10:40:53 +0300

    Update app.py

--- Bart--App-main/app.py
+++ Bart--App-main/app.py
@@ -3,23 +3,6 @@ import time
 import streamlit as st
 import streamlit.components.v1 as components
 
-# ========================================================
-# GLOBAL TRANSLATION WIDGET (THIRD-PARTY)
-# ========================================================
-components.html("""
-<div id="google_translate_element"></div>
-<script type="text/javascript">
-function googleTranslateElementInit() {
-  new google.translate.TranslateElement({
-    pageLanguage: 'en', 
-    includedLanguages: 'ar',
-    layout: google.translate.TranslateElement.InlineLayout.SIMPLE
-  }, 'google_translate_element');
-}
-</script>
-<script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
-""", height=50)
-
 
 
 

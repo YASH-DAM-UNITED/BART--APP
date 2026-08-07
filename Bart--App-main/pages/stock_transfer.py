3c8d1b358089c3d6b4019d16ebf9c608a29c2439
YASH <data@damunited.sa>
2026-08-07 21:22:56 +0300

    Update stock_transfer.py

--- Bart--App-main/pages/stock_transfer.py
+++ Bart--App-main/pages/stock_transfer.py
@@ -396,7 +396,7 @@ if st.session_state.transfer_cart:
     
     # 2. Only show the confirmation button if a destination is selected
     if destination:
-        if st.button("Confirm and Send All", key="confirm_btn", on_click=disable_button, disabled=st.session_state.get('is_submitting', False)):
+        if st.button("Confirm and Send All", key="confirm_btn",  disabled=st.session_state.get('is_submitting', False)):
             
             
             jeddah_time = datetime.now() + timedelta(hours=3)
